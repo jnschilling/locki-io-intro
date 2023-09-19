c32485cb592e74b3a092a8b1a47d890d1f20a815
Jean-Noël Schilling <jeannoel.schilling@gmail.com>
2023-09-19 10:25:27 +0200

    trying to add a 3d element in background

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
 import * as THREE from 'three';
-import { OBJLoader } from 'OBJLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import Stats from 'three/addons/libs/stats.module.js'
 
-// Create a new OBJLoader instance
-const loader = new OBJLoader();
 // Set up the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 2
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement)
 document.getElementById('scene-container').appendChild(renderer.domElement);
-
+const controls = new OrbitControls(camera, renderer.domElement)
+// Create a new OBJLoader instance
+const loader = new GLTFLoader();
 // Load your OBJ model
-loader.load('./assets/mvx_logo.obj', (obj) => {
-    scene.add(obj);
-});
-
-// Set up animation
-const animate = () => {
-    requestAnimationFrame(animate);
-    // Add animation logic here, e.g., obj.rotation.x += 0.01;
-    renderer.render(scene, camera);
-};
-
-camera.position.z = 5;
-animate();
+loader.load('/models/mvx_logo.glb', function (gltf) {
+    gltf.scene.traverse(function (child) {
+        if (child.type === 'Mesh') {
+            let m = child
+            m.receiveShadow = true
+            m.castShadow = true
+        }
+        if (child.type === 'SpotLight') {
+            let l = child
+            l.castShadow = true
+            l.shadow.bias = -0.003
+            //l.shadow.mapSize.width = 2048
+            //l.shadow.mapSize.height = 2048
+        }
+    })
+    progressBar.style.display = 'none'
+    scene.add(gltf.scene)
+    // gltf.animations; // Array<THREE.AnimationClip>
+    // gltf.scene; // THREE.Group
+    // gltf.scenes; // Array<THREE.Group>
+    // gltf.cameras; // Array<THREE.Camera>
+    // gltf.asset; // Object
+    },
+    (xhr) => {
+        if (xhr.lengthComputable) {
+            var percentComplete = (xhr.loaded / xhr.total) * 100
+            progressBar.value = percentComplete
+            progressBar.style.display = 'block'
+        }
+    },
+    (error) => {
+        console.log(error)
+    } 
+);
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+var animate = function () {
+    requestAnimationFrame(animate)
+    controls.update()
+    render()
+    stats.update()
+}
+function render() {
+    renderer.render(scene, camera)
+}
+animate()
